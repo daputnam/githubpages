34b5b266d02a7b53b033fbfde9150df1d4b7a84c
Danny P <daniel.a.putnam@gmail.com>
2019-01-09 19:38:14 -0600

    Updates

---
+++ precache-manifest.b57202e45d7fee143998d591b5d52358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ded08b4b20c19104b52",
+    "url": "/githubpages/static/css/main.77460e4b.chunk.css"
+  },
+  {
+    "revision": "1ded08b4b20c19104b52",
+    "url": "/githubpages/static/js/main.1ded08b4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/githubpages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "6356d00ca95f60244b84",
+    "url": "/githubpages/static/js/runtime~main.6356d00c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubpages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0d36eeed6e432e1de110e2b2b43a2842",
+    "url": "/githubpages/index.html"
+  }
+];
